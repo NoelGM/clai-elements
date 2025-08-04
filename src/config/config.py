33fd2c2ebc775f993055d411632bd32b6d1581f8
García Medel <ngarciam@minsait.com>
2025-08-04 15:28:32 +0200

    Managing dynaconf environments

--- src/config/config.py
+++ src/config/config.py
@@ -5,5 +5,5 @@ from dynaconf import Dynaconf
 config = Dynaconf(
     settings_files=[os.environ.get('CONFIG_FILE')],
     environments=True,
-    env='local'
+    env=os.environ.get('ENVIRONMENT')
 )
